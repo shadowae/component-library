7ea44b2260532a1e332333092e5abac98930be08
Satish K <satish.appanasamy@gmail.com>
2023-08-14 18:10:34 +0800

    feature: updated app to use table component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 import TableComponent from "./TableComponent";
 import "./App.css";
+import sortingAlgo from './sorting.ts'
+const { sortDataAscending,sortDataDescending } = sortingAlgo;
 
 const App: React.FC = () => {
-    const data = [
+    const sampleData = [
         { id: 1, name: "John", age: 25 },
         { id: 2, name: "Jane", age: 30 },
         // ... other data
     ];
+    const [data, setData] = useState(sampleData)
     
     const columns = [
         { name: "ID", key: "id" },
         { name: "Name", key: "name" },
         { name: "Age", key: "age" },
     ];
+    const [selectedRows, setSelectedRows] = useState<any[]>([]);
     
-    const handleSort = (columnKey: string) => {
-        // Implement sorting logic here
-        console.log(columnKey)
+    const handleSort = (columnKey: string, sortDirection: string | null) => {
+        let sortedData: any[] = [...data];
+        
+        if (sortDirection === 'asc') {
+            sortedData = sortDataAscending(sortedData, columnKey);
+        } else if (sortDirection === 'desc') {
+            sortedData = sortDataDescending(sortedData, columnKey);
+        }
+        setData(sortedData)
     };
     
-    const handleSelect = (selectedRows: any[]) => {
+    const handleSelect = (row: any[]) => {
         // Implement selection logic here
-        console.log(selectedRows)
-        
+        if (selectedRows.includes(row)) {
+            setSelectedRows(selectedRows.filter(item => item !== row));
+        } else {
+            setSelectedRows([...selectedRows, row]);
+        }
     };
     
     return (
@@ -32,7 +45,7 @@ const App: React.FC = () => {
                 data={data}
                 columns={columns}
                 sortable={true}
-                selectable="multi"
+                selectable="single"
                 onSort={handleSort}
                 onSelect={handleSelect}
             />
